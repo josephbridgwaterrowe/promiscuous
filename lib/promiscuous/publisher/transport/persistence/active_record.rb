3f950cdba766cc2ab335c21974e9dff15ed588a5
Joseph Bridgwater-Rowe <freakyjoe@gmail.com>
2015-08-14 11:20:43 -0700

    ActiveRecord Migration Checks
    Move the ActiveRecord schema checks into a CLI driven process.
    
    Add the CLI command "migrations", callable using the command line
    "bundle exec promiscuous migrations".
    
    This new CLI command checks for the presence of the transport_collection
    table in the database and that all publishers and subscribers have
    the necessary "_v" column.

--- lib/promiscuous/publisher/transport/persistence/active_record.rb
+++ lib/promiscuous/publisher/transport/persistence/active_record.rb
@@ -2,19 +2,19 @@ class Promiscuous::Publisher::Transport::Persistence::ActiveRecord
   def save(batch)
     check_schema
 
-    q = "INSERT INTO #{table} (batch) " +
-      "VALUES ('#{batch.dump}')"
+    query = "INSERT INTO #{table} (batch) VALUES ('#{batch.dump}')"
 
-    batch.id = connection.insert_sql(q, 'Promiscuous Recovery Save')
+    batch.id = connection.insert_sql(query, 'Promiscuous Recovery Save')
   end
 
   def expired
     check_schema
 
-    q = "SELECT id, p.batch FROM #{table} p " +
-      "WHERE at < CURRENT_TIMESTAMP - INTERVAL '#{Promiscuous::Config.recovery_timeout}' second"
+    query = "SELECT id, p.batch FROM #{table} p " \
+            'WHERE at < CURRENT_TIMESTAMP - INTERVAL ' \
+            "'#{Promiscuous::Config.recovery_timeout}' second"
 
-    connection.exec_query(q, 'Promiscuous Recovery Expired').rows
+    connection.exec_query(query, 'Promiscuous Recovery Expired').rows
   end
 
   def delete(batch)
@@ -28,19 +28,7 @@ class Promiscuous::Publisher::Transport::Persistence::ActiveRecord
   private
 
   def check_schema
-    return if @schema_checked
-
-    unless connection.table_exists?(table)
-      raise <<-help
-        Promiscuous requires the following migration to be run:
-          create_table :_promiscuous do |t|
-            t.text      :batch
-            t.timestamp :at, 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP'
-          end
-      help
-    end
-
-    @schema_checked = true
+    true
   end
 
   def connection
